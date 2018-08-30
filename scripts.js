2627c95379e6eee946fcb7236855ecf1c23308f0
Szatan <manfred666@gmail.com>
2018-08-30 21:18:37 +0200

    Poprawki graficzne

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 window.onload = function(){
-    
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if(!isChrome){
+        removeElement('iframeAudio');    
+    } else {
+        removeElement('playAudio');    
+    }
+}
+
+function removeElement(id) {
+    var element = document.getElementById(id);
+    element.parentNode.removeChild(element);
 }
